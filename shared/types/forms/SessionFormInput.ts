eb1210d1c7c6252edec9314034da80413e96c315
Kurtyyy597 <kurtmarquez238@gmail.com>
2026-06-17 17:37:42 +0800

    refactor: move form types

--- shared/types/forms/SessionFormInput.ts
+++
@@ -1,4 +0,0 @@
-import { createSessionSchema } from './../../../backend/src/schemas/forms/createSessionSchemas';
-import {z} from "zod"
-
-export type SessionFormInput = z.input<typeof createSessionSchema>;
